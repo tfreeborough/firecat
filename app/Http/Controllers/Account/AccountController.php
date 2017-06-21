71687bafb5f0536d51eb597f5d07df00b33581b1
Thomas Freeborough <thomas@hotsnapper.com>
2017-06-22 00:49:41 +0100

    [Accounts] Allow accounts to have additional information + avatars

--- app/Http/Controllers/Account/AccountController.php
+++ app/Http/Controllers/Account/AccountController.php
@@ -10,7 +10,12 @@ namespace App\Http\Controllers\Account;
 
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use JD\Cloudder\Facades\Cloudder;
+use Mockery\CountValidator\Exception;
 
 class AccountController extends Controller
 {
@@ -50,13 +55,91 @@ class AccountController extends Controller
     public function showAccount()
     {
         if($this->user->isAdmin()){
-            return view('account.admin');
+            return view('account.admin',[
+                'user' => $this->user,
+            ]);
         }else if($this->user->isVendor()){
-            return view('account.vendor');
+            return view('account.vendor', [
+                'user' => $this->user,
+            ]);
         }else if($this->user->isPartner()){
-            return view('account.partner');
+            return view('account.partner', [
+                'user' => $this->user,
+            ]);
         }else{
             return redirect('/logout');
+        }
+    }
+
+    public function redirectAccount()
+    {
+        if($this->user->isAdmin()){
+            return redirect('/admin/account');
+        }else if($this->user->isVendor()){
+            return redirect('/vendor/account');
+        }else if($this->user->isPartner()){
+            return redirect('/partner/account');
+        }else{
+            return redirect('/logout');
+        }
+    }
+
+    public function postAvatar(Request $request)
+    {
+        Validator::make($request->all(), [
+            'avatar' => 'required|image',
+        ])->validate();
+
+        $user = User::find(Auth::user()->id);
+
+        if($user->extra->avatar_id){
+            Cloudder::delete($user->extra->avatar_id);
+            $user->extra->avatar_url = null;
+            $user->extra->avatar_id = null;
+        }
+
+        $file = $request->file('avatar');
+        $result = Cloudder::upload($file->getRealPath());
+
+        $user->extra->avatar_id = $result->getPublicId();
+        $user->extra->avatar_url = $result->secureShow($result->getPublicId());
+        $user->extra->save();
+
+        return $this->redirectAccount();
+    }
+
+    public function postAdditional(Request $request)
+    {
+        $user = User::find(Auth::user()->id);
+
+        try{
+            if($request->has('secondary_email')){
+                Validator::make([$request->get('secondary_email')], ['secondary_email' => 'email|max:255'])->validate();
+                if($request->get('secondary_email') === $user->email){
+                    throw new Exception('You cannot have a secondary email the same as your primary email');
+                }
+                $user->extra->second_email = $request->get('secondary_email');
+            }
+
+            if($request->has('work_phone')){
+                Validator::make([$request->get('work_phone')], ['work_phone' => 'string|max:255'])->validate();
+                $user->extra->work_number = $request->get('work_phone');
+            }
+
+            if($request->has('mobile_phone')){
+                Validator::make([$request->get('mobile_phone')], ['mobile_phone' => 'string|max:255'])->validate();
+                if($request->get('mobile_phone') === $request->get('work_phone')){
+                    throw new Exception('Your mobile number cannot be the same as your work number');
+                }
+                $user->extra->mobile_number = $request->get('mobile_phone');
+            }
+
+            $user->extra->save();
+            return $this->redirectAccount();
+        }catch(Exception $e){
+            return $this->redirectAccount()->withErrors([
+                $e->getMessage()
+            ]);
         }
     }
 
--- resources/views/account/admin.blade.php
+++ resources/views/account/admin.blade.php
@@ -20,5 +20,78 @@
                 </ul>
             </div>
         </div>
+        <div id="account-settings">
+            @if (count($errors) > 0)
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div id="create-end-user-errors" class="text-left">
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{!! $error !!}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+            <div class="row">
+                <div class="col-xs-12 col-sm-6">
+                    <h3>Additional Information</h3>
+                    {!! Form::open(['url' => '/admin/account/additional']) !!}
+                    <div class="form-group">
+                        {{ Form::label('secondary_email', null, ['class' => 'control-label']) }}
+                        {{ Form::text('secondary_email', $user->extra->second_email, array_merge(['class' => 'form-control', 'placeholder' => 'This can be a personal email if you wish'])) }}
+                    </div>
+                    <div class="form-group">
+                        {{ Form::label('work_phone', null, ['class' => 'control-label']) }}
+                        {{ Form::text('work_phone', $user->extra->work_number, array_merge(['class' => 'form-control', 'placeholder' => 'Enter your most used work telephone number'])) }}
+                    </div>
+                    <div class="form-group">
+                        {{ Form::label('mobile_phone', null, ['class' => 'control-label']) }}
+                        {{ Form::text('mobile_phone', $user->extra->mobile_number, array_merge(['class' => 'form-control', 'placeholder' => 'This can be a personal mobile if you wish'])) }}
+                    </div>
+                    <div class="form-group">
+                        {{ Form::submit('Update account', array_merge(['class' => 'form-control button action'])) }}
+                    </div>
+                    {!! Form::close() !!}
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <h3>Update my avatar</h3>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6">
+                            {!! Form::open(['url' => '/admin/account/avatar', 'files' => true]) !!}
+                            <div class="form-group">
+                                {{ Form::label('avatar', null, ['class' => 'control-label']) }}
+                                {{ Form::file('avatar', null, array_merge(['class' => 'form-control'])) }}
+                            </div>
+                            <div class="form-group">
+                                {{ Form::submit('Upload Avatar', array_merge(['class' => 'form-control button action'])) }}
+                            </div>
+                            {!! Form::close() !!}
+                        </div>
+                        <div class="col-xs-12 col-sm-6">
+                            @if($user->extra->avatar_id)
+                                <div class="avatar">
+                                </div>
+                            @else
+                                <div class="avatar">
+                                    <img class="default-avatar" src="/images/avatar.png" />
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>
+@endsection
+@section('scripts')
+    <script>
+        @if($user->extra->avatar_id)
+                $('#account-settings .avatar').html($.cloudinary.image('{{ $user->extra->avatar_id }}', { width: 128, height: 128, crop: 'fill', gravity: 'face' }));
+        @endif
+    </script>
 @endsection
