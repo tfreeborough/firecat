68db5318397b96a5487af0418de0b257cb4d4b16
Thomas Freeborough <thomas@hotsnapper.com>
2017-06-22 00:53:03 +0100

    [404] Add better 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,8 +8,21 @@
 @section('content')
     <div id="dashboard">
         <div id="not-found">
-            <h2>We couldn't find the page you were looking for...</h2>
-            <h2>Sorry :(</h2>
+            <div id="not-found-content">
+                <div id="menu-logo">
+                    <a href="{{route('dashboard')}}">
+                        <span class="fire">Page not found</span>
+                    </a>
+                </div>
+                <p>Oops, looks like that page doesn't exist. Here's what you can do now.</p>
+                <ul id="not-found-links">
+                    <a href="{{route('dashboard')}}">
+                        <li>
+                            Go to my dashboard
+                        </li>
+                    </a>
+                </ul>
+            </div>
         </div>
     </div>
 @endsection
