174b8abe6da289c49693f1c91c9ba461d06b828d
Thomas Freeborough <thomas@hotsnapper.com>
2017-11-14 15:36:50 +0000

    [Menu] Add logo to unauthenticated menu

--- resources/views/_partials/menu.blade.php
+++ resources/views/_partials/menu.blade.php
@@ -1,4 +1,9 @@
 <div id="menu">
+    <div id="menu-logo" class="pull-left">
+        <a href="{{route('dashboard')}}">
+            <span class="fire">FIRE</span><span class="cat">CAT</span>
+        </a>
+    </div>
     <div id="menu-links" class="pull-right">
         <ul>
             @if(Auth::user())
