873def5d4c2d8903627e0aa11290120e015480d0
Thomas Freeborough <thomas@hotsnapper.com>
2017-12-09 01:20:23 +0000

    [Organisation] Add onboarding to vendor admin

---
+++ app/Http/Controllers/Vendor/Admin/OnboardingController.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tom
+ * Date: 29/11/2017
+ * Time: 15:16
+ */
+
+namespace App\Http\Controllers\Vendor\Admin;
+
+
+use App\Http\Controllers\Controller;
+use App\Mail\InviteUser;
+use App\Models\Invite;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+use Webpatser\Uuid\Uuid;
+
+class OnboardingController extends Controller
+{
+
+
+    public function showOnboarding()
+    {
+        return view('vendor.admin.onboarding.onboarding')->with([
+            'vendor' => Auth::user()->organisation,
+            'invites' => Auth::user()->organisation->invites
+        ]);
+    }
+    
+    public function postInvite(Request $request, $uuid)
+    {
+        Validator::make($request->all(), [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+        ])->validate();
+
+        if(Auth::user()->isVendorAdministrator($uuid)){
+            $invite = Invite::create([
+                'first_name' => $request['first_name'],
+                'last_name' => $request['last_name'],
+                'email' => $request['email'],
+                'admin' => false,
+                'vendor' => true,
+                'partner' => false,
+                'organisation_id' => $uuid,
+                'organisation_admin' => !is_null($request['admin']),
+                'token' => Uuid::generate(),
+                'expiry' => Carbon::now()->addDays(7)
+            ]);
+
+            Mail::to($request['email'])->send(new InviteUser($invite));
+
+            return redirect(route('vendor.admin.onboarding'))->with([
+                'alert-success' => 'That user has been successfully invited to the system.'
+            ]);
+        }
+
+        return redirect(route('vendor.admin.onboarding'))->withErrors([
+            'alert-danger' => 'You are no an administrator of this organisation.'
+        ]);
+
+    }
+
+    public function deleteInvite($uuid, $invite_id)
+    {
+        if(Auth::user()->isVendorAdministrator($uuid)){
+            $invite = Invite::find($invite_id);
+            if($invite && $invite->organisation->id === $uuid){
+                $invite->delete();
+                return redirect(route('vendor.admin.onboarding'))->with([
+                    'alert-success' => 'That invite has been successfully deleted.'
+                ]);
+            }
+        }
+        return redirect(route('vendor.admin.onboarding'))->withErrors([
+            'alert-danger' => 'You are no an administrator of this organisation.'
+        ]);
+    }
+
+    public function renewInvite($uuid, $invite_id)
+    {
+        if(Auth::user()->isVendorAdministrator($uuid)){
+            $invite = Invite::find($invite_id);
+            if($invite && $invite->organisation->id === $uuid){
+                $invite->token = Uuid::generate();
+                $invite->expiry = Carbon::now()->addDays(7);
+                $invite->save();
+
+                Mail::to($invite->email)->send(new InviteUser($invite));
+
+                return redirect(route('vendor.admin.onboarding'))->with([
+                    'alert-success' => 'That invite has been successfully renewed and a new email sent to '.$invite->email.'.'
+                ]);
+            }
+        }
+        return redirect(route('vendor.admin.onboarding'))->withErrors([
+            'alert-danger' => 'You are no an administrator of this organisation.'
+        ]);
+    }
+}
