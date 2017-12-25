a79b2b3de177aaf709c384cc6827dc213d6fb172
Thomas Freeborough <thomas@hotsnapper.com>
2017-12-25 00:54:36 +0000

    [Deals] Allow partners to request an implementation date change

--- app/Http/Controllers/Partner/DealController.php
+++ app/Http/Controllers/Partner/DealController.php
@@ -9,9 +9,13 @@
 namespace App\Http\Controllers\Partner;
 
 
+use App\Events\CreateOpportunityActivity;
 use App\Http\Controllers\Controller;
 use App\Models\Deal;
+use App\Models\DealUpdate;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class DealController extends Controller
 {
@@ -26,10 +30,47 @@ class DealController extends Controller
     public function showDeal($uuid)
     {
         $deal = Deal::find($uuid);
-        return view('partner.deals.deal', [
-            'deal' => $deal,
-            'opportunity' => $deal->opportunity
+        if($deal !== null && Auth::user()->hasOpportunity($deal->opportunity->id)){
+            return view('partner.deals.deal', [
+                'deal' => $deal,
+                'opportunity' => $deal->opportunity
+            ]);  
+        }
+        return redirect(route('partner.deals.index'))->withErrors([
+            'alert-warning' => 'You do not have access to view that deal'
         ]);
+    }
+
+
+    public function postRequestImplementationDateChange(Request $request, $uuid)
+    {
+        Validator::make($request->all(), [
+            'implementation_date' => 'required|string',
+            'type' => 'required',
+            'formatted_type' => 'required'
+        ])->validate();
+
+        $deal = Deal::find($uuid);
+        if($deal !== null && Auth::user()->hasOpportunity($deal->opportunity->id)){
+            $deal_update = new DealUpdate();
+            $deal_update->deal_id = $uuid;
+            $deal_update->user_id = Auth::user()->id;
+            $deal_update->type = $request->get('type');
+            $deal_update->type_formatted = $request->get('formatted_type');
+            $deal_update->proposal = $request->get('implementation_date');
+            $deal_update->save();
+
+            event(new CreateOpportunityActivity(
+                $deal->opportunity,
+                Auth::user(),
+                Auth::user()->first_name.' '.Auth::user()->last_name.' requested to change the '.$request->get('formatted_type').' of this Deal.',
+                null
+            ));
+
+            return response(200);
+        }
+        $request->session()->flash('alert-error','There was a problem modifying this Deal Reg');
+        return abort(404);
     }
 
 }
