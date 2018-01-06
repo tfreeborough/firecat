411ebe983570d377d52c3ab822827c9391a58e83
Thomas Freeborough <thomas@hotsnapper.com>
2018-01-06 02:50:43 +0000

    [Partner] Add partner dashboard

--- app/Http/Controllers/Partner/PartnerController.php
+++ app/Http/Controllers/Partner/PartnerController.php
@@ -20,6 +20,23 @@ class PartnerController extends Controller
     
     public function showDashboard()
     {
-        return view('partner.dashboard');
+        $deals = Auth::user()->deals;
+        $deals_won = 0;
+        $deals_lost = 0;
+        foreach ($deals as $deal){
+            if($deal->status->won){
+                $deals_won++;
+            }elseif(!$deal->status->pending && !$deal->status->won){
+                $deals_lost++;
+            }
+        }
+
+        return view('partner.dashboard')->with([
+            'deals' => $deals,
+            'opportunities' => Auth::user()->opportunities,
+            'deals_won' => $deals_won,
+            'deals_lost' => $deals_lost,
+            'win_rate' => (100/count($deals))*$deals_won
+        ]);
     }
 }
--- resources/views/partner/dashboard.blade.php
+++ resources/views/partner/dashboard.blade.php
@@ -7,7 +7,7 @@
         <div id="page-topper">
             <div id="page-topper-bg"></div>
             <h1 id="page-title">Dashboard</h1>
-            <h5 id="page-subtitle">Where the magic happens.</h5>
+            <h5 id="page-subtitle">Your deals at a glance.</h5>
             <div id="page-topper-breadcrumbs">
                 <ul>
                     <li>
@@ -16,10 +16,61 @@
                 </ul>
             </div>
         </div>
+        <div class="row">
+            <div class="col-xs-12">
+                @include('_partials.flash_message')
+            </div>
+        </div>
         <div id="partner-dashboard">
-            <div class="row">
-                <div class="col-xs-12">
-                    @include('_partials.flash_message')
+            <div class="top-wrap block">
+                <h3 class="title">Welcome, {{ Auth::user()->first_name }}</h3>
+                <div class="alert alert-info">
+                    <h4>Notice</h4>
+                    Firecat is still in it's early stages of development. We hope that whilst using the service you do not encounter
+                    any issues but if you do, you can reach us directly at <strong>support@firecat.io.</strong> We take feedback
+                    very seriously and endeavour to give you the best way to manage deal registrations.
+                </div>
+            </div>
+            <div id="overview">
+                <div class="dashboard-panel">
+                    <div class="dashboard-panel-wrapper">
+                        <div class="dashboard-panel-big">
+                            {{ count($opportunities) }}/{{ count($deals) }}
+                        </div>
+                        <div class="dashboard-panel-small">
+                            Opportunities/Deals
+                        </div>
+                    </div>
+                </div>
+                <div class="dashboard-panel">
+                    <div class="dashboard-panel-wrapper">
+                        <div class="dashboard-panel-big">
+                            {{ (100/count($opportunities))*count($deals) }}%
+                        </div>
+                        <div class="dashboard-panel-small">
+                            Deal Conversion Rate
+                        </div>
+                    </div>
+                </div>
+                <div class="dashboard-panel">
+                    <div class="dashboard-panel-wrapper">
+                        <div class="dashboard-panel-big">
+                            <span class="green">{{ $deals_won }}</span>/<span class="red">{{ $deals_lost }}</span>
+                        </div>
+                        <div class="dashboard-panel-small">
+                            Deals Won/Lost
+                        </div>
+                    </div>
+                </div>
+                <div class="dashboard-panel">
+                    <div class="dashboard-panel-wrapper">
+                        <div class="dashboard-panel-big">
+                            {{ $win_rate }}%
+                        </div>
+                        <div class="dashboard-panel-small">
+                            Win Rate
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
