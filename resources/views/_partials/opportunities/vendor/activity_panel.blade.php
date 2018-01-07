16f3969de6d8bae17ae635cb8f372f1eda7f26d3
Thomas Freeborough <thomas@hotsnapper.com>
2018-01-07 00:20:38 +0000

    [Activity] Remove activity link

--- resources/views/_partials/opportunities/vendor/activity_panel.blade.php
+++ resources/views/_partials/opportunities/vendor/activity_panel.blade.php
@@ -17,13 +17,5 @@
                 </li>
             @endforeach
         </ul>
-        @if(Auth::user()->isAssigned($opportunity->id))
-            <div id="button-wrapper">
-                <a href="/vendor/opportunities/{{$opportunity->id}}/messages">
-                    <button class="button"><i class="fa fa-thumb-tack" aria-hidden="true"></i> View Full Activity</button>
-                </a>
-            </div>
-        @endif
-
     </div>
 </div>
