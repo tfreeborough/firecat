6226ebd2637c3d530f9b57ed063bb5720a4af5d2
Tom Freeborough <tom.freeborough@thrivelearning.com>
2019-11-12 15:15:33 +0000

    Update tag.blade.php
    Swap icon from broken chain to eye

--- resources/views/vendor/tags/tag.blade.php
+++ resources/views/vendor/tags/tag.blade.php
@@ -85,7 +85,7 @@
                         </td>
                         <td>
                             <a href="/vendor/deals/{{$deal_tag->deal->id}}">
-                                <i class="fa fa-chain-broken" aria-hidden="true"></i>
+                                <i class="fa fa-eye" aria-hidden="true"></i>
                             </a>
                         </td>
                     </tr>
